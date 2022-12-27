7cfdc41feda4bd88df54b859b66536c4a9775a03
VivekGujale <vdgujale007@.com>
2022-12-27 20:54:38 +0530

    Checking valid contacts

--- AddressBookMain.js
+++ AddressBookMain.js
@@ -1,4 +1,14 @@
 console.log("Welcome to Address Book")
+
+const firstNameRegex = RegExp('[A-Z][a-z]{2,}');
+const lastNameRegex = RegExp('[A-Z][a-z]{2,}');
+const addressRegex = RegExp('[a-zA-z0-9#,]{4,}');
+const cityRegex = RegExp('[a-zA-z]{4,}');
+const stateRegex = RegExp('[a-zA-z]{4,}');
+const zipCodeRegex = RegExp("[0-9]{6}");
+const phoneNumberRegex = RegExp("[0-9]{10}");
+const emailRegex = RegExp("[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.,-]+");
+
 class PersonDetails {
 
     constructor(...params) {
@@ -40,31 +50,53 @@ class PersonDetails {
     }
 
     set firstName(firstName) {
-        this._firstName = firstName;
+        if (firstNameRegex.test(firstName))
+            this._firstName = firstName;
+        else
+            throw "First Name is incorrect"
     }
 
     set lastName(lastName) {
-        this._lastName = lastName;
+        if (lastNameRegex.test(lastName))
+            this._lastName = lastName;
+        else
+            throw "Last Name is incorrect"
     }
 
     set city(city) {
-        this._city = city;
+        if (cityRegex.test(city))
+            this._city = city;
+        else
+            throw "City is incorrect"
+
     }
 
     set state(state) {
-        this._state = state;
+        if (stateRegex.test(state))
+            this._state = state;
+        else
+            throw "State is incorrect"
     }
 
     set zip(zip) {
-        this._zip = zip;
+        if (zipCodeRegex.test(zip))
+            this._zip = zip;
+        else
+            throw "Zip code is incorrect"
     }
 
     set phoneNumber(phoneNumber) {
-        this._phoneNumber = phoneNumber;
+        if (phoneNumberRegex.test(phoneNumber))
+            this._phoneNumber = phoneNumber;
+        else
+            throw "Phone number is incorrect"
     }
 
     set email(email) {
-        this._email = email;
+        if (emailRegex.test(email))
+            this._email = email;
+        else
+            throw "Email ID is incorrect"
     }
 
     toString() {
