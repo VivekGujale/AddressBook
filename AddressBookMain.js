d84003a5c26790c5305e007c7793dc5e4c0c1e7a
VivekGujale <vdgujale007@.com>
2022-12-28 10:08:56 +0530

    Creating new contact list by using array

--- AddressBookMain.js
+++ AddressBookMain.js
@@ -6,19 +6,21 @@ const addressRegex = RegExp('[a-zA-z0-9#,]{4,}');
 const cityRegex = RegExp('[a-zA-z]{4,}');
 const stateRegex = RegExp('[a-zA-z]{4,}');
 const zipCodeRegex = RegExp("[0-9]{6}");
-const phoneNumberRegex = RegExp("[0-9]{10}");
+const phoneNumberRegex = RegExp("[91]{2}[ ][0-9]{10}");
 const emailRegex = RegExp("[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.,-]+");
+
 
 class PersonDetails {
 
     constructor(...params) {
         this.firstName = params[0];
         this.lastName = params[1];
-        this.city = params[2];
-        this.state = params[3];
-        this.zip = params[4];
-        this.phoneNumber = params[5];
-        this.email = params[6]
+        this.address = params[2]
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.email = params[7];
     }
 
     get firstName() {
@@ -100,9 +102,18 @@ class PersonDetails {
     }
 
     toString() {
-        return "First Name : " + this.firstName + ", Last Name : " + this.lastName + ", City : " + this.city + ", State : " + this.state + ", Zip : " + this.zip + ", Phone Number : " + this.phoneNumber + ", Email : " + this.email;
+        return "First Name : " + this.firstName + ", Last Name : " + this.lastName + ", Address : "
+            + this.address + ", City : " + this.city + ", State : " + this.state + ", Zip : " + this.zip
+            + ", Phone Number : " + this.phoneNumber + ", Email ID : " + this.email;
     }
 }
 
-let contact = new PersonDetails("Vivek", "Gujale", "Navi Mumbai", "Maharashtra", 410235, 9685654789, "vivek223@gmail.com");
-console.log(contact.toString());
+let addressBookArray = new Array();
+try {
+    addressBookArray.push(new PersonDetails("Vivek", "Gujale", "10-12/21 Shantinagar", "NaviMumbai", "Maharashtra", "410235", "91 9685654789", "vivek223@gmail.com"));
+    addressBookArray.push(new PersonDetails("Sandip", "Kengar", "36-02/15 Gandhinagar", "Pune", "Maharashtra", "425459", "91 9023547852", "sandy25@gmail.com"));
+    addressBookArray.push(new PersonDetails("Sagar", "Kalokhe", "18-20/42 Kantinagar", "Satara", "Maharashtra", "412015", "91 8521456987", "shaggy12@gmail.com"));
+} catch (e) {
+    console.error(e);
+}
+console.log(addressBookArray);
